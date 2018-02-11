11d936fc341ec9b9ef045d1ff917c1842812b07e
enom <satoshi.enom@gmail.com>
2018-02-11 22:16:42 +0900

    Modify: Refactoring

--- prac_unittest.py
+++ prac_unittest.py
@@ -5,39 +5,45 @@ import unittest
 from prac_doctest import fibonacci, fibonacci_memo
 
 
-class TestPracDoctest(unittest.TestCase):
-    def test__fibonacci_right(self):
-        actual1 = fibonacci(0)
-        self.assertEqual(actual1, 0)
+class TestFibonacci(unittest.TestCase):
+    def test__n_eq_0(self):
+        actual = fibonacci(0)
+        self.assertEqual(actual, 0)
 
-        actual2 = fibonacci(1)
-        self.assertEqual(actual2, 1)
+    def test__n_eq_1(self):
+        actual = fibonacci(1)
+        self.assertEqual(actual, 1)
 
-        actual3 = fibonacci(6)
-        self.assertEqual(actual3, 8)
+    def test__n_gte_2(self):
+        actual = fibonacci(6)
+        self.assertEqual(actual, 8)
 
-    def test__fibonacci_negative_number(self):
+    def test__n_lt_0(self):
         self.assertRaises(AssertionError, fibonacci, -1)
 
-    def test__fibonacci_not_int(self):
+    def test__n_not_int(self):
         self.assertRaises(AssertionError, fibonacci, u"Hello World")
         self.assertRaises(AssertionError, fibonacci, 1.2)
         self.assertRaises(AssertionError, fibonacci, -1.0)
 
-    def test__fibonacci_memo_right(self):
-        actual1 = fibonacci_memo(0)
-        self.assertEqual(actual1, 0)
 
-        actual2 = fibonacci_memo(1)
-        self.assertEqual(actual2, 1)
+class TestFibonacciMemo(unittest.TestCase):
+    def test__n_eq_0(self):
+        actual = fibonacci_memo(0)
+        self.assertEqual(actual, 0)
 
-        actual3 = fibonacci_memo(6)
-        self.assertEqual(actual3, 8)
+    def test__n_eq_1(self):
+        actual = fibonacci_memo(1)
+        self.assertEqual(actual, 1)
 
-    def test__fibonacci_memo_negative_number(self):
+    def test__n_gte_2(self):
+        actual = fibonacci_memo(6)
+        self.assertEqual(actual, 8)
+
+    def test__n_lt_0(self):
         self.assertRaises(AssertionError, fibonacci_memo, -1)
 
-    def test__fibonacci_memo_not_int(self):
+    def test__n_not_int(self):
         self.assertRaises(AssertionError, fibonacci_memo, u"Hello World")
         self.assertRaises(AssertionError, fibonacci_memo, 1.2)
         self.assertRaises(AssertionError, fibonacci_memo, -1.0)
